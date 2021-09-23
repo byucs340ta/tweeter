7099e54c2ae374e15aad479e2316eca67f195c17
John <johnmstout32@gmail.com>
2021-09-23 03:26:45 -0600

    Fixed the issues with gradle and such so that it now compiles on my laptop. Just took some code on stack overflow :P

--- app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/BackgroundTaskUtils.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/BackgroundTaskUtils.java
@@ -1,5 +1,5 @@
 package edu.byu.cs.tweeter.client.backgroundTask;
-// todo: change
+// Changed! -Surface
 import android.util.Log;
 
 import java.io.IOException;
@@ -30,4 +30,4 @@ public class BackgroundTaskUtils {
     }
 
 }
-// todo: change
+// Changed! -Surface
