36a082a29d5259c77f2bd3c200ccfdf53ee78ed7
Matt Pope <matt@profusiondev.net>
2021-12-06 13:21:24 -0700

    Add Mockito-android dependency, add default tests

---
+++ app/src/test/java/edu/byu/cs/tweeter/client/TestsWorkingTest.java
@@ -0,0 +1,36 @@
+package edu.byu.cs.tweeter.client;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class TestsWorkingTest {
+    class Foo {
+        public void foo() {
+
+        }
+    }
+
+    @Before
+    public void setup() {
+        // Called before each test, set up any common code between tests
+    }
+
+    @Test
+    public void testAsserts() {
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void testMockitoSpy() {
+        Foo f = Mockito.spy(new Foo());
+        f.foo();
+        Mockito.verify(f).foo();
+    }
+    @Test
+    public void testMockitoMock() {
+        Foo f = Mockito.mock(Foo.class);
+        f.foo();
+        Mockito.verify(f).foo();
+    }
+}
