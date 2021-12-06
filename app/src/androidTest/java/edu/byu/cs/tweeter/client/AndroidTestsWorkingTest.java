ba59656d86afe313073ef1265ef40720a6e48ec4
Matt Pope <matt@profusiondev.net>
2021-12-06 13:28:30 -0700

    Add description comment for purpose of 2 sample tests

--- app/src/androidTest/java/edu/byu/cs/tweeter/client/AndroidTestsWorkingTest.java
+++ app/src/androidTest/java/edu/byu/cs/tweeter/client/AndroidTestsWorkingTest.java
@@ -5,6 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+/**
+ * This class exists purely to prove that tests in your androidTest/java folder have the correct dependencies.
+ * Click on the green arrow to the left of the class declarations to run. These tests should pass if all
+ * dependencies are correctly set up.
+ */
 public class AndroidTestsWorkingTest {
     class Foo {
         public void foo() {
