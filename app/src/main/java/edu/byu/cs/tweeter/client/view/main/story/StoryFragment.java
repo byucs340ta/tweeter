c3b38a68e0ab22c4abf3a476c59f1093d614fb99
matt <matt@profusiondev.net>
2022-09-19 18:29:34 -0600

    Remove unused imports

--- app/src/main/java/edu/byu/cs/tweeter/client/view/main/story/StoryFragment.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/view/main/story/StoryFragment.java
@@ -1,7 +1,6 @@
 package edu.byu.cs.tweeter.client.view.main.story;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
