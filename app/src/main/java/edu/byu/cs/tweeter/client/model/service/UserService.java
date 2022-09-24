d37313145e6965d3e2ea4f3a4bf4d8c5c7e3efa9
Dallin Stevens <stevens.dallin23@gmail.com>
2022-09-24 12:20:22 -0600

    Working Refactorization of Login Fragment

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -1,4 +1,62 @@
 package edu.byu.cs.tweeter.client.model.service;
 
+import android.os.Handler;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import edu.byu.cs.tweeter.client.backgroundTask.LoginTask;
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
 public class UserService {
+
+    public interface GetLoginObserver {
+        void displayErrorMessage(String message);
+        void displayException(Exception ex);
+        void loginUser(User loggedInUser);
+    }
+
+    public void loginUser(String alias, String password, GetLoginObserver observer) {
+        // Send the login request.
+        LoginTask loginTask = new LoginTask(alias, password, new LoginHandler(observer));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(loginTask);
+    }
+
+    /**
+     * Message handler (i.e., observer) for LoginTask
+     */
+    private class LoginHandler extends Handler {
+
+        private UserService.GetLoginObserver observer;
+
+        public LoginHandler(UserService.GetLoginObserver observer) {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(LoginTask.SUCCESS_KEY);
+            if (success) {
+                User loggedInUser = (User) msg.getData().getSerializable(LoginTask.USER_KEY);
+                AuthToken authToken = (AuthToken) msg.getData().getSerializable(LoginTask.AUTH_TOKEN_KEY);
+                // Cache user session information
+                Cache.getInstance().setCurrUser(loggedInUser);
+                Cache.getInstance().setCurrUserAuthToken(authToken);
+                observer.loginUser(loggedInUser);
+
+            } else if (msg.getData().containsKey(LoginTask.MESSAGE_KEY)) {
+                String message = msg.getData().getString(LoginTask.MESSAGE_KEY);
+                observer.displayErrorMessage(message);
+            } else if (msg.getData().containsKey(LoginTask.EXCEPTION_KEY)) {
+                Exception ex = (Exception) msg.getData().getSerializable(LoginTask.EXCEPTION_KEY);
+                observer.displayException(ex);
+            }
+        }
+    }
 }
---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
@@ -0,0 +1,63 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class LoginPresenter {
+
+    // MARK: - Class Variables
+    private View view;
+    private UserService service;
+
+    // MARK: - Interfaces
+    public interface View {
+        void displayMessage(String message);
+        void setLoggingInToast(boolean value);
+        void postLoginUser(User loggedInUser);
+    }
+
+    // MARK: - Constructors
+    public LoginPresenter(View view) {
+        this.view = view;
+        service = new UserService();
+    }
+
+    // MARK: - Class Methods
+    public void login(String alias, String password) {
+        view.setLoggingInToast(true);
+        service.loginUser(alias, password, new GetLoginObserver());
+    }
+
+    public void validateLogin(String alias, String password) {
+        if (alias.charAt(0) != '@') {
+            throw new IllegalArgumentException("Alias must begin with @.");
+        }
+        if (alias.length() < 2) {
+            throw new IllegalArgumentException("Alias must contain 1 or more characters after the @.");
+        }
+        if (password.length() == 0) {
+            throw new IllegalArgumentException("Password cannot be empty.");
+        }
+    }
+
+    // MARK: - Inner Classes
+    private class GetLoginObserver implements UserService.GetLoginObserver {
+
+        @Override
+        public void displayErrorMessage(String message) {
+            view.displayMessage("Failed to login: " + message);
+        }
+
+        @Override
+        public void displayException(Exception ex) {
+            view.displayMessage("Failed to login because of exception: " + ex.getMessage());
+        }
+
+        @Override
+        public void loginUser(User loggedInUser) {
+            view.postLoginUser(loggedInUser);
+            view.setLoggingInToast(false);
+            view.displayMessage("Hello" + loggedInUser.getName());
+        }
+    }
+}
